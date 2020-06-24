89ffe1bacdaac69543e87b1250713341d44e37b0
Artem <iamryabinov@gmail.com>
2020-06-24 22:40:05 +0300

    playing with classsifier

--- run.py
+++ run.py
@@ -1,12 +1,22 @@
 from dataset import *
 import pickle
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import plot_confusion_matrix
+import numpy as np
 
 iemo = Dataset('Iemocap', IEMOCAP_FOLDER, 'English')
-cremad = Dataset('Crema-d', CREMAD_FOLDER, 'English')
-emodb = Dataset('Emo-DB', EMODB_FOLDER, 'German')
-ravdess = Dataset('Ravdess', RAVDESS_FOLDER, 'English')
-savee = Dataset('Savee', SAVEE_FOLDER, 'English')
-tess = Dataset('Tess', TESS_FOLDER, 'English')
+X = iemo.features.X
+y = iemo.features.y.ravel()
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y)
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(X_train, y_train)
+plot_confusion_matrix(knn, X_test, y_test, normalize='true')
+plt.show()
+
+
+
 
 
 
