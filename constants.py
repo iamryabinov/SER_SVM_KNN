dcbf9cc24cb8fc8a9a1967a2e6080b1fdb83eb82
= <=>
2020-06-25 18:09:45 +0300

    knn classifier

--- constants.py
+++ constants.py
@@ -1,5 +1,6 @@
 EXE_PATH = 'tools\\opensmile-2.3.0\\bin\Win32\SMILExtract_Release.exe'
 CONFIG_PATH = 'tools\\opensmile-2.3.0\\config\\gemaps\\eGeMAPSv01a.conf'
+
 IEMOCAP_FOLDER = 'datasets\\iemocap_audios\\'
 CREMAD_FOLDER = 'datasets\\crema-d_audios\\'
 EMODB_FOLDER = 'datasets\\emodb\\wav\\'
