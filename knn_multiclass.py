e377a1a4170592b3e8be71079b8e22a214e01d21
= <=>
2020-06-26 19:13:46 +0300

    cleanup

--- knn_multiclass.py
+++ knn_multiclass.py
@@ -12,32 +12,32 @@ def knn_multi_classification(datasets_list):
         X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, stratify=y)
         print('\nWORKING WITH DATASET {}...'.format(dataset.name))
         print('\n{} training samples; {} test samples.'.format(len(y_train), len(y_test)))
-        n_neighbors_list = np.arange(2, 300, 2)
+        n_neighbors_list = np.arange(2, 101)
+        n_neighbors_list_fact = []
         train_score_list = []
         test_score_list = []
-        _n_neighbors_list = []
         for k in n_neighbors_list:
             try:
                 knn = KNeighborsClassifier(n_neighbors=k)
                 knn.fit(X_train, y_train)
-                train_score = knn.score(X_train, y_train)
-                test_score = knn.score(X_test, y_test)
-                train_score_list.append(train_score)
-                test_score_list.append(test_score)
-                _n_neighbors_list.append(k)
+                train_score_list.append(knn.score(X_train, y_train))
+                test_score_list.append(knn.score(X_test, y_test))
+                n_neighbors_list_fact.append(k)
                 print('K-NN with {} neighbors, train score: {}, test score: {}'.format(k, train_score, test_score))
             except ValueError:
-                print('Value Error! Breaking...')
+                print('Value Error! Too many neighbors! Breaking...')
                 break
         print('Writing to file...')
-        df = pd.DataFrame(data=[train_score_list, test_score_list], index=['train_scores', 'test_scores'],
-                          columns=_n_neighbors_list)
-        df.to_csv('classifiers\\knn\\{}_train_test_score.csv'.format(dataset.name.lower()))
+        data = {'n_neighbors': n_neighbors_list_fact, 
+                'train_score': train_score_list, 
+                'test_score': test_score_list
+                }
+        df = pd.DataFrame(data, columns=['n_neighbors', 'train_score', 'test_score'])
+        df.to_csv('classifiers\\knn\\{}_train_test_score.csv'.format(dataset.name.lower()), index=False, sep=';')
         print('Job done!')
 
 
 if __name__ == '__main__':
-
     iemo = Dataset('Iemocap', IEMOCAP_FOLDER, 'English')
     emodb = Dataset('Emo-DB', EMODB_FOLDER, 'German')
     ravdess = Dataset('Ravdess', RAVDESS_FOLDER, 'English')
