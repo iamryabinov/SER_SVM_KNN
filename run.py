45164fb66efa182e84b6e6676e74f2c8c2bde7c3
Artem <iamryabinov@gmail.com>
2020-06-26 19:19:55 +0300

    merge

--- run.py
+++ run.py
@@ -2,3 +2,14 @@
 
 
 
+from knn_multiclass import *  
+    
+iemo = Dataset('Iemocap', IEMOCAP_FOLDER, 'English')
+emodb = Dataset('Emo-DB', EMODB_FOLDER, 'German')
+ravdess = Dataset('Ravdess', RAVDESS_FOLDER, 'English')
+cremad = Dataset('Crema-D', CREMAD_FOLDER, 'English')
+savee = Dataset('SAVEE', SAVEE_FOLDER, 'English')
+tess = Dataset('TESS', TESS_FOLDER, 'English')
+datasets_list = [iemo, emodb, ravdess, cremad, savee, tess]
+
+knn_multi_classification(datasets_list)
