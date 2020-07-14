1fa9b71dd814c9aa9ef34894eb7e8dd682d1732a
Artem <iamryabinov@gmail.com>
2020-07-14 21:03:10 +0300

    work

--- visualize.py
+++ visualize.py
@@ -1,12 +1,14 @@
 import seaborn as sns
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+import matplotlib
 import pandas as pd
 
-
-sns.set(style="ticks")
+matplotlib.use('Qt5Agg')
+sns.set_style("white")
+sns.set_context("paper")
 results = pd.read_csv('classifiers\\knn\\knn_summarized_results_final.csv', delimiter=';')
 g = sns.relplot(x="n_neighbors", y="score",
                 hue="subset", style="subset", col="dataset", col_wrap=3,
-                legend='brief', height=20, aspect=.5,
-                kind="line", data=results)
+                legend='brief', height=3, aspect=1,
+                kind="line", data=results, palette="PuBuGn_d")
 plt.show()
