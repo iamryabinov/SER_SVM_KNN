175534b90e8205d711728318a29467624beccef3
Artem <iamryabinov@gmail.com>
2020-08-18 00:40:51 +0300

    fix all shit, add some files, tired

--- tsne.py
+++ tsne.py
@@ -1,11 +1,11 @@
-# from dataset import *
-# from sklearn.manifold import TSNE
-# from sklearn.model_selection import train_test_split
+from dataset import *
+from sklearn.manifold import TSNE
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import pandas as pd
-# from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 import matplotlib as mpl
 
 
@@ -129,13 +129,56 @@ def create_tsne_file():
     df = pd.concat(tsne_dfs, ignore_index=True)
     df.to_csv('tsne_results.csv', sep=';', index=False)
 
+def tsne_for_assembly():
+    tsne_dfs = []
+    dataset_perplexity = {
+        iemo_original: 70,
+        cremad_original: 22,
+        all_english_six_original: 71,
+        emodb_original: 37,
+        ravdess_original: 41,
+        savee_original: 21,
+        tess_original: 37
+    }
+    for dataset in [all_english_six_original]:
+        print('\nWORKING WITH {}'.format(dataset.name))
+        dataset.name = dataset.name.split('_')[0].lower()
+        X, y = dataset.features.X, dataset.features.y.ravel()
+        X = StandardScaler().fit_transform(X)
+        if len(y) > 1001:
+            X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, stratify=y, test_size=5000)
+            X = X_test
+            y = y_test
+            print('Fetched data')
+        perplexity = dataset_perplexity[dataset]
+        tsne = TSNE(perplexity=perplexity,
+                    random_state=0,
+                    n_iter=5000,
+                    n_iter_without_progress=2000,
+                    init='pca',
+                    learning_rate=0.1,
+                    early_exaggeration=2)
+        print('Fitting TSNE (perplexity = {})...'.format(perplexity))
+        tsne_obj = tsne.fit_transform(X)
+        print('Creating Dataframe...')
+        tsne_df = pd.DataFrame({'Dataset': dataset.name,
+                                'X': tsne_obj[:, 0],
+                                'Y': tsne_obj[:, 1],
+                                'Label type': 'Descrete',
+                                'Label': y})
+        tsne_dfs.append(tsne_df)
+        print('Appended')
+    print('Concatenating...')
+    df = pd.concat(tsne_dfs, ignore_index=True)
+    df.to_csv('tsne_results_assembly.csv', sep=';', index=False)
+
 def visualize_tsne():
     mpl.use('Qt5Agg')
     data = pd.read_csv('tsne_results.csv', delimiter=';')
-    data = data.loc[data['Label type'] == 'Binary']
+    data = data.loc[data['Label type'] == 'Descrete']
     data = data.loc[data['Dataset'] != 'English Assembly Six']
     col_order = ['Crema-D', 'Emo-DB', 'IEMOCAP', 'RAVDESS', 'SAVEE', 'TESS']
-    hue_order = ['ang', 'hap', 'neu', 'sad', 'dis', 'fea', 'sur', 'cal', 'exc', 'fru', 'bor', 'neg', 'rest']
+    # hue_order = ['ang', 'hap', 'neu', 'sad', 'dis', 'fea', 'sur', 'cal', 'exc', 'fru', 'bor', 'neg', 'rest']
     sns.set(font="Arial", style='white', context='paper', font_scale=1.5)
     palette = {'ang': '#FF0000',
                'hap': '#23FF00',
@@ -156,7 +199,7 @@ def visualize_tsne():
         'rest': '+'
     }
     g = sns.relplot(x="X", y="Y",
-                    hue="Label", hue_order=hue_order, palette=palette,
+                    hue="Label", palette=palette,
                     col='Dataset', col_wrap=3, col_order=col_order,
                     legend='full', height=4, aspect=1,
                     kind="scatter", data=data, facet_kws={'sharex': False, 'sharey': False},
@@ -175,5 +218,5 @@ def visualize_tsne():
 
 
 if __name__ == '__main__':
-    visualize_tsne()
+    tsne_for_assembly()
 
